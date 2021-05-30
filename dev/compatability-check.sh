c49df62b791d453e3c65b651f27dc0f8ddce2dec
Hunter T <strangeranger01@randomserver.xyz>
2021-05-29 19:30:43 -0700

    fix: fix incorrectly punctuated words

--- dev/compatability-check.sh
+++ dev/compatability-check.sh
@@ -4,9 +4,7 @@
 #
 ################################################################################
 
-GREEN=$'\033[0;32m'
 CYAN=$'\033[0;36m'
-RED=$'\033[1;31m'
 NC=$'\033[0m'
 
 echo "Creating temp user 'tester'..."
@@ -15,30 +13,30 @@ echo "Create password for 'root' user..."
 sudo passwd root
 
 
-echo -e "\n\nPerforming iNCorrect password tests...\n"
+echo -e "\n\nPerforming incorrect password tests...\n"
 
-echo "${CYAN}Provide iNCorrect password${NC}"
+echo "${CYAN}Provide incorrect password${NC}"
 echo "Command: 'su'"
 su
-echo "${CYAN}Provide iNCorrect password${NC}"
+echo "${CYAN}Provide incorrect password${NC}"
 echo "Command: 'su root'"
 su root
-echo "${CYAN}Provide iNCorrect password${NC}"
+echo "${CYAN}Provide incorrect password${NC}"
 echo "Command: 'su tester'"
 su tester
-echo "${CYAN}Provide iNCorrect password${NC}"
+echo "${CYAN}Provide incorrect password${NC}"
 echo "Command: 'sudo su'"
 sudo su
-echo "${CYAN}Provide iNCorrect password${NC}"
+echo "${CYAN}Provide incorrect password${NC}"
 echo "Command: 'sudo su root'"
 sudo su root
-echo "${CYAN}Provide iNCorrect password${NC}"
+echo "${CYAN}Provide incorrect password${NC}"
 echo "Command: 'sudo su tester'"
 sudo su tester
-echo "${CYAN}Provide iNCorrect password${NC}"
+echo "${CYAN}Provide incorrect password${NC}"
 echo "Command: 'sudo -i'"
 sudo -i
-echo "${CYAN}Provide iNCorrect password${NC}"
+echo "${CYAN}Provide incorrect password${NC}"
 echo "Command: 'sudo bash'"
 sudo bash
 
